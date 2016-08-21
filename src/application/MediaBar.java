4f1bd1a87f6cd0bae40a8e3fdd460916c8a6d98a
RaquelRedondo <raquelro22@gmail.com>
2016-08-21 21:22:11 +0100

    Added listeners to time and volume sliders

--- src/application/MediaBar.java
+++ src/application/MediaBar.java
@@ -1,5 +1,10 @@
 package application;
 
+import javafx.application.Platform;
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
@@ -8,6 +13,7 @@ import javafx.scene.control.Slider;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaPlayer.Status;
 
 public class MediaBar extends HBox{
 	
@@ -37,6 +43,62 @@ public class MediaBar extends HBox{
 		getChildren().add(time);
 		getChildren().add(playButton);
 		getChildren().add(volume);
+		
+		playButton.setOnAction(new EventHandler<ActionEvent>(){
+			public void handle(ActionEvent e){
+				Status status = player.getStatus();
+				
+				if (status == Status.PLAYING){
+					if (player.getCurrentTime().greaterThanOrEqualTo(player.getTotalDuration())){
+					player.seek(player.getStartTime());	
+					} else {
+						playButton.setText(">");
+						player.pause();
+					}
+						
+				} 
+				if (status != Status.PLAYING){
+					player.play();
+					playButton.setText("||");
+				}
+			}
+		});
+		
+		//Add listener to update the value of the time slider
+		player.currentTimeProperty().addListener(new InvalidationListener(){
+			
+			public void invalidated(Observable ob){
+				updateValues();
+			}
+		});
+		
+		//Make the video jump to specific specified place in slider
+		time.valueProperty().addListener(new InvalidationListener(){
+
+			public void invalidated(Observable ob){
+				if (time.isPressed()){
+					player.seek(player.getMedia().getDuration().multiply(time.getValue()/100));
+				}
+			}
+		});
+		
+		//Change the volume of the player according to vol slider value
+		vol.valueProperty().addListener(new InvalidationListener(){
+			
+			public void invalidated(Observable ob){
+				if (vol.isPressed()){
+					player.setVolume(vol.getValue()/100);
+				}
+			}
+		});
+	}
+	
+	protected void updateValues(){
+		Platform.runLater(new Runnable(){
+			public void run(){
+				time.setValue(player.getCurrentTime().toMillis()/player.getTotalDuration().toMillis()*100);
+			}
+		});
 	}
 
 }
